ccacc96dfe522a9ef76cb8dbc57dc50b70b87145
Nils Walker <49301769+wauchi@users.noreply.github.com>
2019-08-11 18:20:55 +0200

    Added intervals when the page needs to be reloaded

--- eair-bot.js
+++ eair-bot.js
@@ -8,3 +8,33 @@
 // @grant        none
 // ==/UserScript==
 
+// Restart game after certain purpose
+// Purpose: successful run
+setInterval(
+	function() {
+		if(document.getElementsByClassName("question-number")[0].innerText === "1 / 10") {
+			location.reload();
+		}
+	},
+	1250
+);
+
+// Purpose: error, elements not set
+setInterval(
+	function() {
+		if(typeof (document.getElementsByClassName("title-verification")[0]) == 'undefined' && document.getElementsByClassName("title-verification")[0] == null) {
+			location.reload();
+		}
+	},
+	7500
+);
+
+// Purpose: Not all questions were answered right
+setInterval(
+	function() {
+		if(document.getElementsByTagName("h1")[0].innerText === "Leider verloren") {
+			location.reload();
+		}
+	},
+	1000
+);
