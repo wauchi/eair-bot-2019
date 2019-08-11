971e957266ace3d8f63cd73e0ba9ce3042f029ef
Nils Walker <49301769+wauchi@users.noreply.github.com>
2019-08-11 18:13:13 +0200

    Added UserScript so TamperMonkey can read it

---
+++ eair-bot.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name EAir bot
+// @namespace    https://github.com/wauchi/eair-bot-2019/blob/master/eair-bot.js
+// @version      1.1
+// @description Bot for the eair game
+// @author       wauchi
+// @match        https://game.energy.ch
+// @grant        none
+// ==/UserScript==
+
