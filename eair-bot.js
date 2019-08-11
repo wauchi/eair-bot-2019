cce5be1cf2509aa929506544327b2605d518d7c0
Nils Walker <49301769+wauchi@users.noreply.github.com>
2019-08-11 18:44:41 +0200

    Wrote method to answer question and added all q and a in a array

--- eair-bot.js
+++ eair-bot.js
@@ -8,6 +8,37 @@
 // @grant        none
 // ==/UserScript==
 
+let question_and_answer = [
+    ["Die wievielte Energy Air Ausgabe findet dieses Jahr statt?", "Die sechste"],
+    ["Wie viele Mitarbeiter sind am Energy Air im Einsatz?", "1300"],
+    ["Energy Air Tickets kann man…", "gewinnen"],
+    ["Wie viele Konfetti-Kanonen gibt es am Energy Air?", "60"],
+    ["Mit welchem dieser Tickets geniesst du die beste Sicht zur Energy Air Bühne?", "XTRA-Circle"],
+    ["Auf welcher Social-Media-Plattform kann man keine Energy Air Tickets gewinnen?", "Twitter"],
+    ["Auf welchem Weg kann man KEINE Energy Air Tickets gewinnen?", "E-Mail"],
+    ["Wann fand das Energy Air zum ersten Mal statt?", "2014"],
+    ["Wie schwer ist die Energy Air Bühne?", "450 Tonnen"],
+    ["Welche amerikanische Band trat am Energy Air 2016 auf?", "One Republic"],
+    ["Wie breit ist die Energy Air Bühne?", "70 Meter"],
+    ["Wer war der letzte Act am Energy Air 2018?", "Lo & Leduc"],
+    ["Wo erfährst du immer die neusten Infos rund um das Energy Air?", "im Radio, auf der Website und über Social Media"],
+    ["Wie viele Energy Air Tickets werden verlost?", "40’000"],
+    ["Was passiert, wenn es am Eventtag regnet?", "Energy Air findet trotzdem statt"],
+    ["Wie reiste Kygo im Jahr 2015 ans Energy Air?", "Im Privatjet"],
+    ["Welche DJ-Acts standen 2018 auf der Bühne des Energy Air?", "Averdeck"],
+    ["Wo findet das Energy Air statt?", "Stade de Suisse, Bern"],
+    ["Wer eröffnete das erste Energy Air?", "Bastian Baker"],
+    ["Wann beginnt das Energy Air 2019?", "Um 17 Uhr"],
+    ["Wie viele Spotlights gibt es am Energy Air?", "250"],
+    ["Welcher dieser Acts hatte einen Auftritt am Energy Air 2018?", "Alvaro Soler"],
+    ["Welche Fussballmannschaft ist im Stade de Suisse zuhause?", "BSC Young Boys"],
+    ["Wie viele Acts waren beim letzten Energy Air dabei?", "14"],
+    ["Energy Air ist der einzige Energy Event, …", "...der unter freiem Himmel stattfindet."],
+    ["Wen nahm Knackeboul am Energy Air 2014 mit backstage?", "Sein Mami"],
+    ["Was verlangte Nena am Energy Air 2016?", "Eine komplett weisse Garderobe"],
+    ["Wann findet das Energy Air 2019 statt?", "7. September 2019"]
+];
+
 // Restart game after certain purpose
 // Purpose: successful run
 setInterval(
@@ -38,3 +69,28 @@ setInterval(
 	},
 	1000
 );
+
+// Function to get a random number between 1000 and 1
+function getRandomNum() {
+	return Math.floor(Math.random() * -999 + 1000);
+}
+
+function answer() {
+    let question = document.getElementsByClassName("question-text")[0].innerText;
+    for (let i =0; i < question_and_answer.length; i++){
+        if (question.toUpperCase() === question_and_answer[i][0].toUpperCase()){
+            if(document.getElementsByClassName("question-number")[0].innerText === "10 / 10"){
+                document.getElementById(question_and_answer[i][1]).click();
+                document.getElementById("next-question").click();
+                setTimeout(function() {CLICK TICKET;} , getRandomNum()); // todo Here need to click on one img to win ticket
+            }else{
+                document.getElementById(question_and_answer[i][1]).click();
+                document.getElementById("next-question").click();
+                setTimeout(function() {answer();} , getRandomNum()); // Execute this method again to answer next question
+            }
+            break;
+        }
+    }
+}
+
+
